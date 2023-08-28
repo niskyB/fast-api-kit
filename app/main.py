c38164828bf874251249033c61a004357f7e0ce9
Loc Nguyen <V_NGUL509@kellyservices.com>
2023-08-28 15:40:36 +0700

    validator

--- app/main.py
+++ app/main.py
@@ -1,10 +1,61 @@
+import json
 import uvicorn
 from fastapi import status
 
 from app.core import http_client
 from app.factory import create_app
+from starlette.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
 
 app = create_app()
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+    if type(exc.detail) is str:
+        return JSONResponse(status_code=exc.status_code, content={"detail": exc.detail})
+    if (
+        exc.detail.get("error_description") is not None
+        or exc.detail.get("error_codes") is not None
+    ):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={
+                "detail": exc.detail.get("error_description"),
+                "error_codes": exc.detail.get("error_codes"),
+            },
+        )
+    if exc.detail.get("detail").get("error") is not None:
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={
+                "detail": exc.detail.get("detail").get("error").get("message"),
+                "error_codes": [exc.detail.get("detail").get("error").get("code")],
+            },
+        )
+    if exc.detail.get("error") is not None:
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={
+                "detail": exc.detail.get("error").get("message"),
+                "error_codes": exc.detail.get("error").get("code"),
+            },
+        )
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.detail.get("detail")},
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    exc_json = json.loads(exc.json())
+    response = {"detail": ""}
+    error = exc_json[0]
+    response["detail"] = error["loc"][-1] + f" {error['msg']}"
+
+    return JSONResponse(response, status_code=422)
 
 
 @app.on_event("startup")
